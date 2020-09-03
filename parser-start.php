b218966770792af6eb01d355a89fcf021e772ab1
Freyze <mcfreyze@gmail.com>
2020-09-03 07:49:39 +0300

    Upload to server

--- parser-start.php
+++ parser-start.php
@@ -71,6 +71,7 @@ if (isset($_POST['start-url'])) {
     sleep(5);
 
     startApi();
+    
 }
 
 function startParser($url) {
@@ -80,7 +81,8 @@ function startParser($url) {
     $content = 'Parser is worked';
     file_put_contents(PARSER_PATH.PARSER_LOCK_FILE, $content);
 
-    echo shell_exec('node index.js '.$url);
+    echo "Starting parser...";
+    shell_exec('node index.js '.$url);
 
 }
 
@@ -91,5 +93,7 @@ function startApi() {
     $content = 'API is worked';
     file_put_contents(API_PATH.API_LOCK_FILE, $content);
 
-    echo shell_exec('php main.php');
+    echo "Starting api...";
+    shell_exec('php main.php');
+
 }
